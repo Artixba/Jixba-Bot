ee0b872f8fef592f4601a39416847b9e4edfba92
Jixba <Artixba@hotmail.com>
2021-01-21 20:39:08 -0600

    improved spam command, now directly dm's target users instead of posting messages in server, handles error cases more effectively.: :

--- commands/spam.js
+++ commands/spam.js
@@ -1,53 +1,70 @@
 const Discord = require("discord.js");
 
-async function messageSpam(user, message){
-  for (let index = 0; index < timelen; index++) {
-    await setTimeout(async() => {
-      if(message) message.channel.send(`${user} ${message}`);
-      else await message.channel.send(`${user} WYA!`);
-    },index * 1500);
-  }
-}
-
 module.exports.run = async (bot, message, args) => {
-    try {
-      console.log(`${message.content} ran.`);
-      
-      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
-      let spamcount = args[1];
-      let spammessage = args.join(' ').slice(25);
-      let everyonemessage = args.join(' ').slice(12);
-      let heremessage = args.join(' ').slice(8)
-      console.log(spammessage);
+  try {
+    console.log(`${message.content} ran.`);
+    
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+    let spamcount = parseInt(args[1]); // '1' 1
+    let spammessage = args.slice(2).join(' ');
 
-      const helpmessage = new Discord.MessageEmbed()
-      .setColor('#02791a')
-      .setTitle('!spam Help')
-      .setAuthor('Artixba')
-      .setDescription('Sends a user customized message to a specified user a declared number of times every 1.5 seconds.')
-      .setThumbnail('https://i.imgur.com/UThwfDM.jpeg')
-      .addField('How to use', '!spam <username> <number of messages> <optional: custom message>')
-      .setImage('https://i.imgur.com/tEMjI8W.jpeg')
-      .setTimestamp();
+    //when you type !spam @user notnumber message
+    console.log(spamcount);
+    console.log(typeof spamcount); //number
+    console.log(typeof spamcount === typeof spammessage); //false
+    console.log(Number.isInteger(spamcount)); //true
+    console.log(typeof spamcount === typeof 1); //true
+    console.log(typeof spamcount !== typeof 1);//false
+    console.log(typeof 1); // number
+    console.log(typeof spamcount === typeof 1); //true
+    console.log(typeof spamcount === typeof 1);//true
 
-      if( args[0] === 'help') return message.channel.send(helpmessage);
-      else if(args[0] === '@everyone') {
-        messageSpam(args[0], everyonemessage);
+    async function messageSpam(user, Message, stop){
+      console.log("I'm in loop");
+
+      if(spamcount > 10){
+        spamcount = 10;
+        message.channel.send('Your number is too high. Spamming 10 times...')
+      } 
+      try {
+        console.log('try block');
+        for (let index = 0; index < spamcount; index++) {
+          console.log('actual loop');
+          await setTimeout(async() => {
+            if(Message === '') return bot.users.cache.get(user.id).send('WYA!');
+            if(Message) return bot.users.cache.get(user.id).send(Message);       
+          },index * 1500);
+        }
+      } catch (error) {
+        message.channel.send('Spam amount not set.')
       }
-      else if(args[0] === '@here'){
-        messageSpam(args[0], heremessage);
-      }
-      else if(!rUser) return message.channel.send("Couldn't find user to spam!");
-      else if(!spamcount) return message.channel.send("Time not set!");
-      else {
-        messageSpam(rUser, spammessage);
-      }
-      console.log(`rUser = ${rUser} and spamcount = ${spamcount}`);
-    } catch (error) {
-      console.log(error);
-    }
+
     }
     
-    module.exports.help = {
-      name: "spam"
+    const helpmessage = new Discord.MessageEmbed()
+    .setColor('#02791a')
+    .setTitle('!spam Help')
+    .setAuthor('Artixba')
+    .setDescription('Sends a user customized message to a specified user a declared number of times every 1.5 seconds.')
+    .setThumbnail('https://i.imgur.com/UThwfDM.jpeg')
+    .addField('How to use', '!spam <username> <number of messages> <optional: custom message>')
+    .setImage('https://i.imgur.com/tEMjI8W.jpeg')
+    .setTimestamp();
+
+    
+    if(args[0] === 'stop') return true;
+    if( args[0] === 'help') return message.channel.send(helpmessage);
+    else if(args[0] === '@everyone' || args[0] === '@here') {
+      return messageSpam(message.author, "Can't spam everyone in server dum dum :middle_finger: ");
     }
+    if(!rUser) return message.channel.send("Couldn't find user to spam!");
+    if(!spamcount) return message.channel.send('Spam amount not set!')
+    if(rUser && spamcount) return messageSpam(rUser, spammessage); 
+  } catch (error) {
+    console.log(error);
+  }
+}
+    
+module.exports.help = {
+  name: "spam"
+}
